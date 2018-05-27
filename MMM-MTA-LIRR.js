bf22b5ede67ac1aa845decb794b6aabd90fe433f
Ndi Sampson <ndisampson@gmail.com>
2018-05-27 18:52:44 -0400

    add MMM-MTA-LIRR.js

---
+++ MMM-MTA-LIRR.js
@@ -0,0 +1,13 @@
+Module.register("helloworld",{
+  // Default module config.
+  defaults: {
+    text: "Hello World!"
+  },
+
+  // Override dom generator.
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = this.config.text;
+    return wrapper;
+  }
+});
